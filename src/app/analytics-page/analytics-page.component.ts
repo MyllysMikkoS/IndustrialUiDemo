6b0363529294551ef16406ab75fc2abaed80a2f9
Mikko Myllys <myllysmikkos@gmail.com>
2022-04-02 23:52:26 +0300

    Analytics page

--- src/app/analytics-page/analytics-page.component.ts
+++ src/app/analytics-page/analytics-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-analytics-page',
   templateUrl: './analytics-page.component.html',
-  styleUrls: ['./analytics-page.component.sass']
+  styleUrls: ['./analytics-page.component.scss']
 })
 export class AnalyticsPageComponent implements OnInit {
 
